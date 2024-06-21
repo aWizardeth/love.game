6e98b9f22a9667898807a14e53d2178907e6e4b6
aWizardeth <awizard.eth@gmail.com>
2024-06-21 09:47:15 -0700

    add deployfix test

--- system/context/FileThemeContext.tsx
+++ system/context/FileThemeContext.tsx
@@ -14,6 +14,7 @@ export interface FileThemeCustomOptions {
   LoveIcon: StaticImageData;
   FireIcon: StaticImageData;
   PaperIcon: StaticImageData;
+  BridgeIcon: StaticImageData;
   UniswapIcon: StaticImageData;
   WalletIcon: StaticImageData;
   SettingsIcon: StaticImageData;
